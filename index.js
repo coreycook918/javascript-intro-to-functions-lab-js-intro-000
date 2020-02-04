5096cf93f9ca30a8e55d341e56fea6221d0b99b8
Corey Cook <cookc7438@rwps.org>
2020-02-04 15:56:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log('hello'.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
+  if (string === 'hello' {
     return 'I can\'t hear you!'
   }
 }
